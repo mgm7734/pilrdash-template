65102ddd94935dca8f7ecdaa73c7cedff6a8f857
tdschenk <schenk.tyler@gmail.com>
2014-06-30 16:28:14 -0500

    First commit

---
+++ R/dashtemplate-package.r
@@ -0,0 +1,14 @@
+#' Dashboard Template
+#' 
+#' This is a template for creating functions to be used with PiLR dashboards.
+#'
+#' @name dashtemplate
+#' @docType package
+#' @param data The dataset retrieved from PiLR
+#' @param params A list of all arguments sent to pilr_dashboard_panel
+
+## Choose a name for your function and write the code for it in place of plot(1:10)
+#' @export
+nameyourfunction <- function(data, params, ...) {
+  plot(1:10)
+}
